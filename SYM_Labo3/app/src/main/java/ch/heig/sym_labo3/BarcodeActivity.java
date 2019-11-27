9b580fe3bf2957f99728165e5cd7372b46c8ec34
Robin Fournier <fournier.robin@gmail.com>
2019-11-27 20:07:13 +0100

    Fully working Labo + Bonus point + Report

--- SYM_Labo3/app/src/main/java/ch/heig/sym_labo3/BarcodeActivity.java
+++ SYM_Labo3/app/src/main/java/ch/heig/sym_labo3/BarcodeActivity.java
@@ -2,13 +2,123 @@ package ch.heig.sym_labo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 
 public class BarcodeActivity extends AppCompatActivity {
+    private String lastText;
+    private static final String TAG = BarcodeActivity.class.getSimpleName();
+    private DecoratedBarcodeView barcodeView;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null || result.getText().equals(lastText)) {
+                // Prevent duplicate scans
+                return;
+            }
+            lastText = result.getText() ;
+            showToast(lastText);
+            setLabel(lastText);
+            barcodeView.setStatusText(result.getText());
+            //Added preview of scanned barcode
+            ImageView imageView = (ImageView) findViewById(R.id.imgPreview);
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.view);
+        Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39);
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        barcodeView.initializeFromIntent(getIntent());
+        barcodeView.decodeContinuous(callback);
+    }
+
+    public void readCode(View view){
+        new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(callback);
+
+    }
+
+    public void setLabel(String s){
+        TextView lblCode = findViewById(R.id.lblCode);
+        if(lblCode != null){
+            lblCode.setText(s);
+        } else {
+            System.err.println("Null lblCode");
+        }
+    }
+
+    public void showToast(String s){
+        Toast.makeText(this, "Scanned: " + s, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
